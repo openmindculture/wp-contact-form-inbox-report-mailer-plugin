05d7e7358f20719fb47c526ae1a7c953f44ed4cc
Ingo Steinke <ingosteinke@gmx.de>
2023-10-03 23:11:00 +0200

    show server time

--- contact-form-inbox-report-mailer/trunk/includes/output-page.php
+++ contact-form-inbox-report-mailer/trunk/includes/output-page.php
@@ -9,6 +9,9 @@ echo 'Next scheduled email report: ';
 if (wp_next_scheduled( OPENMINDCULTURE_CFIRM_SCHEDULE_NAME )) {
 	$openmindculture_cfirm_next_scheduled_time = wp_next_scheduled( OPENMINDCULTURE_CFIRM_SCHEDULE_NAME );
 	echo get_date_from_gmt( date('Y-m-d H:i:s', $openmindculture_cfirm_next_scheduled_time ) );
+	echo ' (server time: ';
+	echo get_date_from_gmt( date('Y-m-d H:i:s' ) );
+	echo ')';
 } else {
 	echo 'not scheduled. (Re)activate the plugin to (re)schedule!';
 }
