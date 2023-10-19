177550108f6b398567d6510d92db1ce4cf2d590e
Ingo Steinke <ingosteinke@gmx.de>
2023-10-19 12:37:54 +0200

    remove debug output

--- contact-form-inbox-report-mailer/trunk/includes/output-page.php
+++ contact-form-inbox-report-mailer/trunk/includes/output-page.php
@@ -5,30 +5,6 @@ echo '<div class="openmindculture__cfirm__wrap">';
 echo '<br>';
 
 echo '  <div class="openmindculture__cfirm__card">';
-echo esc_html(
-	'Next scheduled email report: ',
-	OPENMINDCULTURE_CFIRM_TEXT_DOMAIN
-);
-if (wp_next_scheduled( 'openmindculture_cfirm_schedule' )) {
-	$openmindculture_cfirm_next_scheduled_time = wp_next_scheduled( 'openmindculture_cfirm_schedule' );
-	echo get_date_from_gmt( date('Y-m-d H:i:s', $openmindculture_cfirm_next_scheduled_time ) );
-	echo ' (';
-	echo esc_html(
-		'server time',
-		OPENMINDCULTURE_CFIRM_TEXT_DOMAIN
-	);
-	echo ' ';
-	echo get_date_from_gmt( date('Y-m-d H:i:s' ) );
-	echo ')';
-} else {
-	echo esc_html(
-		'not scheduled. (Re)activate the plugin to (re)schedule!',
-		OPENMINDCULTURE_CFIRM_TEXT_DOMAIN
-	);
-}
-echo '  </div>';
-
-echo '  <div class="openmindculture__cfirm__card">';
 			require_once( plugin_dir_path( __FILE__ ) . 'generate-report.php' );
 			$report = openmindculture_generate_report ();
 			if ( $report && !empty( $report )) :
